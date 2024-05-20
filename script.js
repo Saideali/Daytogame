0e87deb86023d2e4604537794074a7b3ec9a3380
Saide Ali Sefo <saidealisefo2007@gmail.com>
2024-05-20 15:16:35 +0200

    at

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+var menu = document.querySelector('#menu')
 var menubtn = document.querySelector('#menu-btn')
 function menushow(){
-    
+    if(menu.classList.contains('open')){
+        menu.classList.remove('open')
+    }else{
+        menu.classList.add('open')
+    }
 }
